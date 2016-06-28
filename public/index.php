5727264f739836f5f7ddab31e91213a130dc6e72
taodutra <taodutra@gmail.com>
2016-06-28 15:13:25 -0300

    Landing v1

---
+++ public/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class="bg-black">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Democracia ATRFB</title>
+
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        
+        <link href="css/app.css" rel="stylesheet" type="text/css" />
+        <link rel="icon" href="favicon.png?1" type="image/png">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="master">
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.6&appId=225694947474484";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+		<nav class="navbar" id="navbar">
+		    <div class="container">
+
+		        <h1 class="site-title">
+		            <a href="./" title="Democracia ATRFB">Democracia ATRFB</a>
+		        </h1>
+
+		    </div>
+		</nav>
+        
+        <div class="main-wrapper main intro">
+            <a href="" class="history-back mobile-only"></a>
+            <div class="container">
+
+            	<div class="text">
+	
+	            	<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+
+					<h3>Phasellus et tempus leo. Sed tempor tellus in quam consectetur, ut tincidunt elit rhoncus.</h3>
+
+					<p>Vestibulum euismod vel leo in lacinia. Integer eleifend diam vel neque lacinia, sit amet cursus lacus feugiat. Integer at vulputate est, porta rutrum ipsum. Quisque vulputate tellus id neque congue, eget pretium nunc auctor. Etiam vitae urna auctor, vehicula magna sed, euismod leo. Suspendisse ornare euismod nulla, id ultrices nibh efficitur id. Fusce egestas odio non nisi consequat, aliquet euismod nibh volutpat. Fusce nisi massa, dignissim non tellus quis, interdum vestibulum odio. Maecenas volutpat aliquet magna, vel consectetur ante fringilla ac.</p>
+
+            	</div>
+
+            	<div class="fb">
+            		<p class="title"><a href="" title="Manifeste-se!">Manifeste-se!</a></p>
+					<div class="fb-page" data-href="https://www.facebook.com/Democracia-ATRFB-246913679010525" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Democracia-ATRFB-246913679010525" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Democracia-ATRFB-246913679010525">Democracia ATRFB</a></blockquote></div>
+            	</div>
+            </div>
+        </div><!-- ./wrapper -->
+
+        <!-- Footer -->
+        <div class="footer">
+        </div>
+    </body>
+    
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/app.js" type="text/javascript"></script>
+
+</html>
